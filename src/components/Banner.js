efaa521a49b74cef11f7fbdbe460cd230d772f4a
rahathasanDev <devrahat.hasan@gmail.com>
2024-02-09 12:22:13 +0600

    portfolio project with react tailwind

---
+++ src/components/Banner.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Image from '../assets/avatar.svg'
+
+
+const Banner = () => {
+  return (
+    <div className="section" id='home'> 
+    Banner
+  </div>
+ 
+  )
+  
+};
+
+export default Banner;
